8dc381d08d063b862e7d676a0c4f2adcd3643ac1
Moises L. Apocalipse <lopeski@outlook.com>
2021-09-15 09:04:52 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("mysql://hlckdjhhpzc8qdxf:wy29yyqe7uazrr3f@ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xu8vw5mvvivtzpvl");
+const sequelize = new Sequelize("${{ secrets.APIKEY }}");
 
 export default sequelize
